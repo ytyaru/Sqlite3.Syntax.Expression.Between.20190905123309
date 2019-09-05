399b907d699ead9762317571c6af2d899a1d0e93
ytyaru <ytyaru@outlook.jp>
2019-09-05 12:34:47 +0900

    初回コミット

---
+++ src/0.sql
@@ -0,0 +1,7 @@
+select 0 between 0 and 100;
+select -1 between 0 and 100;
+select 100 between 0 and 100;
+select 101 between 0 and 100;
+
+select 'K' between 'A' and 'Z';
+select '200-02-28' between '200-02-01' and '200-03-01';
